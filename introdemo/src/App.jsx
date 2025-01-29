0de42c298fdab5b7079111c37152f46512f17c41
Sun <YSUN98@volvocars.com>
2025-01-29 13:13:35 -0500

    react part 1

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
+import { useState } from 'react'
+
 // First letter of React component names must be capitalized.
 const Header = (props) => {
   console.log(props.course)
 }
 
+// how to pass array of object to a react function
 const Content = (props) => {
   
 }
 
 const App = () => {
-  const name = 'Uting' 
+  const [counter, setCounter ] = useState(0)
+  
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  
   //In React, the individual things rendered in braces must be primitive values, such as numbers or strings.
   
   const course = 'Half Stack application development'
@@ -35,6 +44,8 @@ const App = () => {
           <Header course={course}/>
           <Content parts={parts}/>
         </p>
+        
+        <div>{counter}</div>
     </>
   )
 }
