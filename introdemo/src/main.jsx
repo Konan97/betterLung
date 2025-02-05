f9c6bf6efaa22a128e155b223302b92ca02288e8
Sun <YSUN98@volvocars.com>
2025-02-05 14:42:47 -0500

    promise

--- introdemo/src/main.jsx
+++ introdemo/src/main.jsx
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App.jsx'
-
+// get data from server
+axios
+.get('http://localhost:3001/notes')
+.then(response => {
+    console.log(response)
+    const notes = response.data
+    console.log(notes)
+})
 createRoot(document.getElementById('root')).render(
     <App />
 )
