a43e1fa75fa007e7eeebbe9569a9233d9e832d79
Sun <YSUN98@volvocars.com>
2025-03-04 07:01:23 -0500

    setup connection

--- openAI.js
+++ openAI.js
@@ -3,10 +3,9 @@ import OpenAI from "openai";
 const openai = new OpenAI({
     organization: "org-XGadsTPh0H5a4co8xYU6VQm2",
     project: "proj_EE8ljHCfgUFs9PFQBHLMIACL",
-    apiKey: "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"
 });
-
-async function main() {
+//apiKey: ""
+async function openaiStream() {
     const stream = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: "Say this is a test" }],
@@ -18,4 +17,4 @@ async function main() {
     }
 }
 
-main();
+openaiStream();
